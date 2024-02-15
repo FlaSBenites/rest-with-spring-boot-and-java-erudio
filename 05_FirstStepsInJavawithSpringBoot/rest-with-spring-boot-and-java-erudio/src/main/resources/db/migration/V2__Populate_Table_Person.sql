29bbd09eedcafbd826f77de0b3b6ddb0fc6a2685
Flávio <flaviosbs82@gmail.com>
2024-02-15 08:30:17 -0300

    commit: alterações com o conteudo de Custom JSON Serialization

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'Curitiba','Flávio','Male','Benites'),
+(2,'Colombo - Brasil','Erica','Male','Melo Benites'),
+(5,'Campina Grande do Sul - Brasil','Pedro','Male','Benites'),
+(6,'Campina Grande do Sul - Brasil','Laura','Male','Benites'),
+(7,'Cianorte - Brasil','Laeli','Male','Rodrigues'),
+(8,'Araruna - Brasil','Ernesto','Male','Benites');
